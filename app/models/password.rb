237d8f514688e76088ba998f56f54f777a0fde6c
SzymON <42994488+SzymonSoczewka@users.noreply.github.com>
2022-05-07 09:09:55 +0200

    Comments

--- app/models/password.rb
+++ app/models/password.rb
@@ -8,16 +8,25 @@ class Password < ApplicationRecord
 
   before_save :encrypt_password
   def encrypt_password(encryption_key: Password.encryption_key)
-    cipher = OpenSSL::Cipher.new('aes-256-cbc')
-    cipher.encrypt
-    cipher.key = encryption_key
-    iv = cipher.random_iv
-    cipher.iv = iv
-    encrypted = cipher.update(password.to_s) + cipher.final + iv
-    self.password = [encrypted].pack('m')
+    # 1. Create Cipher instance that uses AES 256 bit encryption with CBC mode
+    cipher = OpenSSL::Cipher.new('aes-256-cbc') 
+    # 2. Initializes the Cipher for encryption/decryption
+    cipher.encrypt 
+    # 3. Set derived key for encryption
+    cipher.key = encryption_key 
+    # 4. Generate random initialization vector
+    iv = cipher.random_iv 
+    # 5. Set initialization vector for encryption
+    cipher.iv = iv 
+    # 6. Encrypt plaintext and append initialization vector
+    encrypted = cipher.update(password.to_s) + cipher.final + iv 
+    # 7. Change the encryption result to Base64 encoded string. 
+    # So that it can be stored in the database
+    self.password = [encrypted].pack('m') 
   end
 
   def decrypt_password(encryption_key: Password.encryption_key)
+    # Change Base64 encoded string to original state
     encrypted_password = password.unpack1('m')
     decipher = OpenSSL::Cipher.new('aes-256-cbc')
     decipher.decrypt
@@ -28,9 +37,11 @@ class Password < ApplicationRecord
   end
 
   def self.change_password(old_password:, new_password:)
+    # Some basic validation of passwords
     raise WrongPasswordError if ENV['USER_PASSWORD'] != old_password && ENV['USER_PASSWORD'] == new_password
-
+    # Turn off callback
     Password.skip_callback(:save, :before, :encrypt_password)
+    # Run entire operation as a transaction
     Password.transaction do
       old_encryption_key = Password.encryption_key
       new_encryption_key = Password.encryption_key(user_password: new_password)
@@ -38,18 +49,25 @@ class Password < ApplicationRecord
       raise WrongPasswordError if file.exclude?("USER_PASSWORD: #{old_password}")
 
       file.sub!(old_password, new_password)
+      # Change user password
       File.write('config/application.yml', file)
       Password.find_each do |password|
+        # For each class instance, decrypt the password with an old encryption key
         password.password = password.decrypt_password(encryption_key: old_encryption_key)
+        # For each class instance, encrypt the password with an new encryption key
         password.encrypt_password(encryption_key: new_encryption_key)
         password.save!
       end
     end
+    # Turn on callback
     Password.set_callback(:save, :before, :encrypt_password)
   end
 
   def self.encryption_key(user_password: ENV['USER_PASSWORD'])
+    # Get the secret_key stored on a user's hard drive
     system_secret_key = File.read(ENV['SECRET_KEY_LOCATION'])
+    # MD5 calculates a digest of 128 bits (16 bytes) 
+    # Based on user's password and system's secret key
     Digest::MD5.hexdigest user_password + system_secret_key
   rescue StandardError => e
     raise KeyDeriviationError
